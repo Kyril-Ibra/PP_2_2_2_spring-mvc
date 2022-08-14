4836eb171abab9fbcccfa8ac8229bde87d01a4f6
Kyril <moreunderground@gmail.com>
2022-08-14 17:13:09 +0300

    fixes

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,18 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 
 @Controller
 @RequestMapping("/")
 public class CarController {
 
-    private final CarServiceImpl carService;
+    private final CarService carService;
 
     @Autowired
-    public CarController(CarServiceImpl carService) {
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class CarServiceImpl implements CarService{
-    private static int CAR_COUNT;
-    private  List<Car> car;
+    private static int car_count;
+    private final List<Car> car;
 
     {
         car = new ArrayList<>();
 
-        car.add(new Car(++CAR_COUNT, "BMW", 5));
-        car.add(new Car(++CAR_COUNT, "BMW", 3));
-        car.add(new Car(++CAR_COUNT, "Rolls Royce", 2));
-        car.add(new Car(++CAR_COUNT, "Tesla", 3));
-        car.add(new Car(++CAR_COUNT, "Porsche", 900));
+        car.add(new Car(++car_count, "BMW", 5));
+        car.add(new Car(++car_count, "BMW", 3));
+        car.add(new Car(++car_count, "Rolls Royce", 2));
+        car.add(new Car(++car_count, "Tesla", 3));
+        car.add(new Car(++car_count, "Porsche", 900));
     }
 
     public List<Car> getCarList(Integer count) {
